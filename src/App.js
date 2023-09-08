083207aa668406cd16322a31ffbd935412ea49af
Armani Araujo <aaraujo3@my.yorku.ca>
2023-09-07 22:42:39 -0400

    Added TODO notes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import { useEffect, useState } from 'react';
 import { WeatherTile } from './WeatherTile';
 import axios from 'axios'
+import { WeatherTileRow } from './WeatherTileRow';
 // require('dotenv').config()
+
+/*
+ * 
+ * TODO: Make cityOptions use lat and long params from geolocator() method
+ * TODO: If geolocation grabber does not work, add dropdown to get country name, then dropdown to get state/location
+ * 
+ * 
+ */
 
 function App() {
 
@@ -9,25 +18,26 @@ function App() {
   const[location, setLocation] = useState(() => { geolocator() })
   const cityOptions = {
     
-    method: 'GET',
-    url: 'http://api.openweathermap.org/geo/1.0/reverse?',
-    params: {
-      lat: 43.595310, 
-      lon: -79.640579, 
-      limit: 1, 
-      appid: 'tempidk'
-    }
+    // method: 'GET',
+    // url: 'http://api.openweathermap.org/geo/1.0/reverse?',
+    // params: {
+    //   lat: 43.595310, 
+    //   lon: -79.640579, 
+    //   limit: 1, 
+    //   appid: 'tempidk'
+    // }
   }
 
   const[city, setCity] = useState(() => {
     axios(cityOptions)
     .then((res) => {
       console.log(res.data)
-      return res.data.name
+      // setCity(res.data[0].name)
+      setCity('TempCity')
     })
     .catch((err) => {
       console.log(err)
-      setCity('Mississauga')
+      setCity('TempCity')
       
     })
   
@@ -72,6 +82,15 @@ function App() {
     console.log('Error: Please enable location permissions to access weather')
   }
 
+  function tileCreator() {
+    const rows = []
+    const numrows = 5
+  for (let i = 0; i < numrows; i++) {
+      rows.push(<WeatherTile/>);
+  }
+  return <tbody>{rows}</tbody>;
+  }
+
   return (
     <div className="App">
       <h1>Hello, World!</h1>
@@ -79,9 +98,13 @@ function App() {
       <h1>You exist in: { city }</h1>
 
       <button></button>
-      <WeatherTile />
+      <div>
+      
+        <WeatherTileRow />
+      </div>
     </div>
   );
 }
+
 
 export default App;
