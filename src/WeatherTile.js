84fe6dbd10c7a6926db1b23d778bf0583e78bd32
Armani Araujo <aaraujo3@my.yorku.ca>
2023-09-14 18:17:35 -0400

    Updated WeatherTile params

--- src/WeatherTile.js
+++ src/WeatherTile.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import styles from './styles/styles.module.css'
 
 export class WeatherTile extends Component {
 
   render() {
-    const {weekday, temp, feels_like} = this.props.day
+    // const {weekday, temp, feels_like} = this.props.day
+    const weatherItem = this.props.day
+    let icon = weatherItem.weather[0].icon
+    console.log(icon)
+    const iconUrl = 'https://openweathermap.org/img/wn/' + icon + '@2x.png'
+    const condition = weatherItem.weather[0].description
+    const temperature = weatherItem.main.temp
 
     return(
       <div>
-        <p>{ weekday }</p>
-        <p>{ temp }</p>
-        <p>{ feels_like }</p>
+        <div className={styles.weathertile}>
+          <img src={iconUrl} alt={condition} />
+          <p>{condition}</p>
+          <p>{Math.round(temperature)}°C</p>
+        </div>
       </div>
     )
   }
